1cf2d935e4f31af92ed0c4890f9676b3496bfe83
David Campuzano <48020477+dcampuzano101@users.noreply.github.com>
2023-10-10 13:50:46 -0400

    trying to resolve conflicts

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,10 +10,19 @@ export const metadata: Metadata = {
 export default function About() {
   return (
     <main>
+<<<<<<< HEAD
       <h1 className='text-xl'>About Us</h1>
       <p className='text-sm'>
         NextSpace is a community of developers and designers who are passionate
         about learning and sharing knowledge.
+=======
+      <h1 className="text-xl">
+        Listen - Testing Again - Testing Again - About Us - With CI/CD
+      </h1>
+      <p className="text-sm">
+        CI-CD With Github Actions is now enabled is a community of developers
+        and designers who are passionate about learning and sharing knowledge.
+>>>>>>> 7a3dee16bca89a06eca62f8e6901c8109a7bb6c3
       </p>
     </main>
   );
