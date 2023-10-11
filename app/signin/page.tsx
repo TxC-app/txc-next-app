d07cd41479a131d8eaf6af5b0d6f4acc66586423
David Campuzano <48020477+dcampuzano101@users.noreply.github.com>
2023-10-10 20:10:11 -0400

    adds prisma, db, auth, user roles & initial schema

---
+++ app/signin/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { SignInCard } from '@/components/SignInCard';
+
+export default function SignIn() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <SignInCard />
+    </div>
+  );
+}
